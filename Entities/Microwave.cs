c4c33cb799c87b6cec54961fb695e0050ca7661b
Chloe Nibali <Chloe.Nibali@edu.sait.ca>
2024-01-31 20:44:09 -0700

    added appliances.txt file. Added comments

--- Entities/Microwave.cs
+++ Entities/Microwave.cs
@@ -16,19 +16,24 @@ namespace Team2_ModernAppliances.Entities
         //Microwave specific attributes
         private string Capacity {  get; set; }
         private RoomType Room { get; set; }
+        
         //microwave ctor
         public Microwave(string id, string brand, int quantity, string wattage, string colour, double price, string capacity, RoomType roomType) :base(id, brand, quantity, wattage, colour, price)
         {
+            // all shared attributes set by parent class ctor
             this.Capacity = capacity;
             this.Room = roomType;
         }
         public override string FormatForFile()
         {
+            // formatted string for file, seperated by ;
             return $"{this.ItemNumber};{this.Brand};{this.Quantity};{this.Wattage};{this.Colour};{this.Price};{this.Capacity};{this.Room};";
         }
         public override string ToString()
         {
+            // calls base to print all shared attribute values
             base.ToString();
+            // then returns microwave specific attributes
             return $"Capacity: {this.Capacity}\n" +
                 $"RoomType: {this.Room}";
         }
