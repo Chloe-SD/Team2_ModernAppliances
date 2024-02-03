d91532a3962e7ad3665f5a7dfbeae5d6fb47b266
Denver Krushel <Denver.Krushel@edu.sait.ca>
2024-02-03 13:00:26 -0700

    Coded Refridgerator class.

--- Entities/Refrigerator.cs
+++ Entities/Refrigerator.cs
@@ -8,5 +8,35 @@ namespace Team2_ModernAppliances.Entities
 {
     internal class Refrigerator:Appliance
     {
+        public int Doors { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public Refrigerator(string itemNumber, string brand, int quantity, string wattage, string colour, double price, int doors, double height, double width) : base(itemNumber, brand, quantity, wattage, colour, price)
+        {
+            this.Doors = doors;
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public override string FormatForFile()
+        {
+            return $"{this.ItemNumber};{this.Brand};{this.Quantity};{this.Wattage};{this.Colour};{this.Price};{this.Doors};{this.Height};{this.Width}";
+        }
+
+        public override string ToString()
+        {
+            // all appliances need to have a ToString. but you can call base as part of the output
+            // and just add in the additional details specific to appliance type
+            return $"ItemNumber: {this.ItemNumber}\n" +
+                $"Brand: {this.Brand}\n" +
+                $"Quantity: {this.Quantity}\n" +
+                $"Wattage: {this.Wattage}\n" +
+                $"Colour: {this.Colour}\n" +
+                $"Price: {this.Price}\n" +
+                $"Doors: {this.Doors}\n" +
+                $"Height: {this.Height}\n" +
+                $"Width: {this.Width}";
+        }
     }
 }
