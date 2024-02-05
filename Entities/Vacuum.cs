04df85728e201e61935f8f5baa4701d01b5fa83d
Chloe Nibali <Chloe.Nibali@edu.sait.ca>
2024-02-05 11:48:36 -0700

    merged all branches

--- Entities/Vacuum.cs
+++ Entities/Vacuum.cs
@@ -8,5 +8,24 @@ namespace Team2_ModernAppliances.Entities
 {
     internal class Vacuum:Appliance
     {
+        private int batteryVoltage { get; set; }
+        private string grade { get; set; }
+
+        public Vacuum(string itemNumber, string brand, int quantity, string wattage, string colour, double price, string grade, int batteryVoltage) : base(itemNumber, brand, quantity, wattage, colour, price)
+        {
+            batteryVoltage = this.batteryVoltage;
+            grade = this.grade;
+        }
+
+        public override string FormatForFile()
+        {
+            return $"{this.ItemNumber}:{this.Brand}:{this.Quantity}:{this.Wattage}:{this.Colour}:{this.Price}:{this.grade}:{this.batteryVoltage}";
+        }
+
+        public string ToString()
+        {
+            base.ToString();
+            return $"Grade: {this.grade} \nBattery Voltage {this.batteryVoltage}";
+        }
     }
 }
